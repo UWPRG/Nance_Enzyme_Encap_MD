345ed1aa3d58f4ee34045617aaa5537fce53582a
Josh <jsmith07@uw.edu>
2018-05-04 10:16:02 -0700

    Initialize VCS.

---
+++ .env/lib/python3.6/site-packages/mdtraj/geometry/distance.py
@@ -0,0 +1,287 @@
+##############################################################################
+# MDTraj: A Python Library for Loading, Saving, and Manipulating
+#         Molecular Dynamics Trajectories.
+# Copyright 2012-2015 Stanford University and the Authors
+#
+# Authors: Robert McGibbon
+# Contributors: Kyle A Beauchamp, Jason Swails
+#
+# MDTraj is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation, either version 2.1
+# of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with MDTraj. If not, see <http://www.gnu.org/licenses/>.
+##############################################################################
+
+
+
+from __future__ import print_function, division
+import numpy as np
+from mdtraj.utils import ensure_type
+from mdtraj.utils.six.moves import range
+from . import _geometry
+
+
+__all__ = ['compute_distances', 'compute_displacements',
+           'compute_center_of_mass', 'find_closest_contact']
+
+
+
+def compute_distances(traj, atom_pairs, periodic=True, opt=True):
+    """Compute the distances between pairs of atoms in each frame.
+
+    Parameters
+    ----------
+    traj : Trajectory
+        An mtraj trajectory.
+    atom_pairs : np.ndarray, shape=(num_pairs, 2), dtype=int
+        Each row gives the indices of two atoms involved in the interaction.
+    periodic : bool, default=True
+        If `periodic` is True and the trajectory contains unitcell
+        information, we will compute distances under the minimum image
+        convention.
+    opt : bool, default=True
+        Use an optimized native library to calculate distances. Our optimized
+        SSE minimum image convention calculation implementation is over 1000x
+        faster than the naive numpy implementation.
+
+    Returns
+    -------
+    distances : np.ndarray, shape=(n_frames, num_pairs), dtype=float
+        The distance, in each frame, between each pair of atoms.
+    """
+    xyz = ensure_type(traj.xyz, dtype=np.float32, ndim=3, name='traj.xyz', shape=(None, None, 3), warn_on_cast=False)
+    pairs = ensure_type(atom_pairs, dtype=np.int32, ndim=2, name='atom_pairs', shape=(None, 2), warn_on_cast=False)
+    if not np.all(np.logical_and(pairs < traj.n_atoms, pairs >= 0)):
+        raise ValueError('atom_pairs must be between 0 and %d' % traj.n_atoms)
+
+    if len(pairs) == 0:
+        return np.zeros((len(xyz), 0), dtype=np.float32)
+
+    if periodic and traj._have_unitcell:
+        box = ensure_type(traj.unitcell_vectors, dtype=np.float32, ndim=3, name='unitcell_vectors', shape=(len(xyz), 3, 3),
+                          warn_on_cast=False)
+        orthogonal = np.allclose(traj.unitcell_angles, 90)
+        if opt:
+            out = np.empty((xyz.shape[0], pairs.shape[0]), dtype=np.float32)
+            _geometry._dist_mic(xyz, pairs, box.transpose(0, 2, 1).copy(), out, orthogonal)
+            return out
+        else:
+            return _distance_mic(xyz, pairs, box.transpose(0, 2, 1), orthogonal)
+
+    # either there are no unitcell vectors or they dont want to use them
+    if opt:
+        out = np.empty((xyz.shape[0], pairs.shape[0]), dtype=np.float32)
+        _geometry._dist(xyz, pairs, out)
+        return out
+    else:
+        return _distance(xyz, pairs)
+
+
+def compute_displacements(traj, atom_pairs, periodic=True, opt=True):
+    """Compute the displacement vector between pairs of atoms in each frame of a trajectory.
+
+    Parameters
+    ----------
+    traj : Trajectory
+        Trajectory to compute distances in
+    atom_pairs : np.ndarray, shape[num_pairs, 2], dtype=int
+        Each row gives the indices of two atoms.
+    periodic : bool, default=True
+        If `periodic` is True and the trajectory contains unitcell
+        information, we will compute distances under the minimum image
+        convention.
+    opt : bool, default=True
+        Use an optimized native library to calculate distances. Our
+        optimized minimum image convention calculation implementation is
+        over 1000x faster than the naive numpy implementation.
+
+    Returns
+    -------
+    displacements : np.ndarray, shape=[n_frames, n_pairs, 3], dtype=float32
+         The displacememt vector, in each frame, between each pair of atoms.
+    """
+    xyz = ensure_type(traj.xyz, dtype=np.float32, ndim=3, name='traj.xyz', shape=(None, None, 3), warn_on_cast=False)
+    pairs = ensure_type(np.asarray(atom_pairs), dtype=np.int32, ndim=2, name='atom_pairs', shape=(None, 2), warn_on_cast=False)
+    if not np.all(np.logical_and(pairs < traj.n_atoms, pairs >= 0)):
+        raise ValueError('atom_pairs must be between 0 and %d' % traj.n_atoms)
+
+    if periodic and traj._have_unitcell:
+        box = ensure_type(traj.unitcell_vectors, dtype=np.float32, ndim=3, name='unitcell_vectors', shape=(len(xyz), 3, 3),
+                          warn_on_cast=False)
+        orthogonal = np.allclose(traj.unitcell_angles, 90)
+        if opt:
+            out = np.empty((xyz.shape[0], pairs.shape[0], 3), dtype=np.float32)
+            _geometry._dist_mic_displacement(xyz, pairs, box.transpose(0, 2, 1).copy(), out, orthogonal)
+            return out
+        else:
+            return _displacement_mic(xyz, pairs, box.transpose(0, 2, 1), orthogonal)
+
+    # either there are no unitcell vectors or they dont want to use them
+    if opt:
+        out = np.empty((xyz.shape[0], pairs.shape[0], 3), dtype=np.float32)
+        _geometry._dist_displacement(xyz, pairs, out)
+        return out
+    return _displacement(xyz, pairs)
+
+
+def compute_center_of_mass(traj):
+    """Compute the center of mass for each frame.
+
+    Parameters
+    ----------
+    traj : Trajectory
+        Trajectory to compute center of mass for
+
+    Returns
+    -------
+    com : np.ndarray, shape=(n_frames, 3)
+         Coordinates of the center of mass for each frame
+    """
+
+    com = np.zeros((traj.n_frames, 3))
+    masses = np.array([a.element.mass for a in traj.top.atoms])
+    masses /= masses.sum()
+
+    for i, x in enumerate(traj.xyz):
+        com[i, :] = x.astype('float64').T.dot(masses)
+    return com
+
+
+def find_closest_contact(traj, group1, group2, frame=0, periodic=True):
+    """Find the closest contact between two groups of atoms.
+
+    Given a frame of a Trajectory and two groups of atoms, identify the pair of
+    atoms (one from each group) that form the closest contact between the two groups.
+
+    Parameters
+    ----------
+    traj : Trajectory
+        An mtraj trajectory.
+    group1 : np.ndarray, shape=(num_atoms), dtype=int
+        The indices of atoms in the first group.
+    group2 : np.ndarray, shape=(num_atoms), dtype=int
+        The indices of atoms in the second group.
+    frame : int, default=0
+        The frame of the Trajectory to take positions from
+    periodic : bool, default=True
+        If `periodic` is True and the trajectory contains unitcell
+        information, we will compute distances under the minimum image
+        convention.
+
+    Returns
+    -------
+    result : tuple (int, int, float)
+         The indices of the two atoms forming the closest contact, and the distance between them.
+    """
+    xyz = ensure_type(traj.xyz, dtype=np.float32, ndim=3, name='traj.xyz', shape=(None, None, 3), warn_on_cast=False)[frame]
+    atoms1 = ensure_type(group1, dtype=np.int32, ndim=1, name='group1', warn_on_cast=False)
+    atoms2 = ensure_type(group2, dtype=np.int32, ndim=1, name='group2', warn_on_cast=False)
+    if periodic and traj._have_unitcell:
+        box = ensure_type(traj.unitcell_vectors, dtype=np.float32, ndim=3, name='unitcell_vectors', shape=(len(traj.xyz), 3, 3),
+                          warn_on_cast=False)[frame]
+    else:
+        box = None
+    return _geometry._find_closest_contact(xyz, atoms1, atoms2, box)
+
+
+##############################################################################
+# pure python implementation of the core routines
+##############################################################################
+
+
+def _distance(xyz, pairs):
+    "Distance between pairs of points in each frame"
+    delta = np.diff(xyz[:, pairs], axis=2)[:, :, 0]
+    return (delta ** 2.).sum(-1) ** 0.5
+
+
+def _displacement(xyz, pairs):
+    "Displacement vector between pairs of points in each frame"
+    value = np.diff(xyz[:, pairs], axis=2)[:, :, 0]
+    assert value.shape == (xyz.shape[0], pairs.shape[0], 3), 'v.shape %s, xyz.shape %s, pairs.shape %s' % (str(value.shape), str(xyz.shape), str(pairs.shape))
+    return value
+
+
+def _reduce_box_vectors(vectors):
+    """Make sure box vectors are in reduced form."""
+    (bv1, bv2, bv3) = vectors
+    bv3 -= bv2*round(bv3[1]/bv2[1]);
+    bv3 -= bv1*round(bv3[0]/bv1[0]);
+    bv2 -= bv1*round(bv2[0]/bv1[0]);
+    return (bv1, bv2, bv3)
+
+
+def _distance_mic(xyz, pairs, box_vectors, orthogonal):
+    """Distance between pairs of points in each frame under the minimum image
+    convention for periodic boundary conditions.
+
+    The computation follows scheme B.9 in Tukerman, M. "Statistical
+    Mechanics: Theory and Molecular Simulation", 2010.
+
+    This is a slow pure python implementation, mostly for testing.
+    """
+    out = np.empty((xyz.shape[0], pairs.shape[0]), dtype=np.float32)
+    for i in range(len(xyz)):
+        bv1, bv2, bv3 = _reduce_box_vectors(box_vectors[i].T)
+
+        for j, (a,b) in enumerate(pairs):
+            r12 = xyz[i,b,:] - xyz[i,a,:]
+            r12 -= bv3*round(r12[2]/bv3[2]);
+            r12 -= bv2*round(r12[1]/bv2[1]);
+            r12 -= bv1*round(r12[0]/bv1[0]);
+            dist = np.linalg.norm(r12)
+            if not orthogonal:
+                for ii in range(-1, 2):
+                    v1 = bv1*ii
+                    for jj in range(-1, 2):
+                        v12 = bv2*jj + v1
+                        for kk in range(-1, 2):
+                            new_r12 = r12 + v12 + bv3*kk
+                            dist = min(dist, np.linalg.norm(new_r12))
+            out[i, j] = dist
+    return out
+
+
+def _displacement_mic(xyz, pairs, box_vectors, orthogonal):
+    """Displacement vector between pairs of points in each frame under the
+    minimum image convention for periodic boundary conditions.
+
+    The computation follows scheme B.9 in Tukerman, M. "Statistical
+    Mechanics: Theory and Molecular Simulation", 2010.
+
+    This is a very slow pure python implementation, mostly for testing.
+    """
+    out = np.empty((xyz.shape[0], pairs.shape[0], 3), dtype=np.float32)
+    for i in range(len(xyz)):
+        bv1, bv2, bv3 = _reduce_box_vectors(box_vectors[i].T)
+        hinv = np.linalg.inv(np.array([bv1, bv2, bv3]).T)
+
+        for j, (a,b) in enumerate(pairs):
+            r12 = xyz[i,b,:] - xyz[i,a,:]
+            r12 -= bv3*round(r12[2]/bv3[2]);
+            r12 -= bv2*round(r12[1]/bv2[1]);
+            r12 -= bv1*round(r12[0]/bv1[0]);
+            min_disp = r12
+            dist2 = (r12*r12).sum()
+            if not orthogonal:
+                for ii in range(-1, 2):
+                    v1 = bv1*ii
+                    for jj in range(-1, 2):
+                        v12 = bv2*jj+v1
+                        for kk in range(-1, 2):
+                            tmp = r12 + v12 + bv3*kk
+                            new_dist2 = (tmp*tmp).sum()
+                            if new_dist2 < dist2:
+                                dist2 = new_dist2
+                                min_disp = tmp
+            out[i, j] = min_disp
+
+    return out
---
+++ add_monomer.py
@@ -0,0 +1,177 @@
+import os.path as op
+from io import StringIO
+
+import pandas as pd
+
+
+data_dir = op.join(op.dirname(__file__), 'top_data/PEG/3/')
+ff_dir = op.join(op.dirname(__file__), 'amber99sb-ildn.ff/')
+
+# give file names for topology and force field
+polymer_topolgy = op.join(data_dir, 'PEG_GMX.top')
+force_field_nonbonded = op.join(ff_dir, 'ffnonbonded.itp')
+force_field_bonded = op.join(ff_dir, 'ffbonded.itp')
+
+
+cap0 = {'name': 'PEG',
+        'list': ['C1', 'H1', 'H2', 'H3', 'O1',
+                 'C2', 'H4', 'H5', 'C3', 'H6', 'H7']
+        }
+
+monomer = {'name': 'PEG',
+           'list': ['O2', 'C4', 'H8', 'H9', 'C5', 'H10', 'H11']
+           }
+
+cap2 = {'name': 'PEGo',
+        'list': ['O3', 'C6', 'H12', 'H13', 'C7',
+                 'H14', 'H15', 'O4', 'H16']
+        }
+
+
+# read file into string for parsing
+def parse_topology(filename, section, header):
+    top_string = ''
+    with open(filename) as top_file:
+        for line in top_file:
+            top_string += line
+    # process section string for conversion to dataframe
+    section_string = top_string.split(section)[1].split('\n\n')[0]
+    pandas_readable = StringIO(section_string)
+    # create section DataFrame
+    section_df = pd.read_table(pandas_readable, sep='\s+',
+                               comment=';', names=header)
+    return section_df
+
+
+# define headers for sections of interest
+atoms_header = ['nr', 'type',  'resi',  'res',
+                'atom', 'cgnr', 'charge', 'mass']
+
+atom_types_header = ['name', 'bond_type', 'mass', 'charge',
+                     'ptype', 'sigma', 'epsilon']
+
+bonds_header = ['ai', 'aj', 'funct', 'r', 'k']
+
+# get polymer topology DataFrames
+atoms = parse_topology(polymer_topolgy, '[ atoms ]', atoms_header)
+bonds = parse_topology(polymer_topolgy, '[ bonds ]', bonds_header)
+atom_types = parse_topology(polymer_topolgy, '[ atomtypes ]',
+                            atom_types_header)
+
+for index, row in atoms.iterrows():
+
+    if row['atom'] in monomer['list']:
+        atoms.loc[index, 'restype'] = monomer['name']
+    elif row['atom'] in cap0['list']:
+        atoms.loc[index, 'restype'] = cap0['name']
+    elif row['atom'] in cap2['list']:
+        atoms.loc[index, 'restype'] = cap2['name']
+
+
+
+# get force field topology DataFrames
+ff_bonds = parse_topology(force_field_bonded, '[ bondtypes ]',
+                          bonds_header)
+ff_atom_types = parse_topology(force_field_nonbonded, '[ atomtypes ]',
+                               atom_types_header)
+
+
+for index, row in atom_types.iterrows():
+    # attempt to assign atom type based on LJ parameters
+    candidates = ff_atom_types.ix[
+        (ff_atom_types['sigma'] == row['sigma'])
+        & (ff_atom_types['epsilon'] == row['epsilon'])]
+
+    # holler if there are no matches!
+    if candidates.empty:
+        print('Ooops! Could not find match for atom type {} in '
+              'this forcefield! Add this atom manually.'.format(row['name']))
+        continue
+    # assign atom type if there is only match
+    if len(candidates) == 1:
+        atoms.loc[atoms['type'] == row['name'], 'ff_name'] = candidates['name'].values[0]
+        continue
+
+
+    atom_number = atoms[atoms['type'] == row['name']]['nr'].values[0]
+    atom_names = str(
+        atoms[atoms['type'] == row['name']]['atom'].values
+    ).replace('[', '').replace(']', '')
+
+    bond_check = bonds.ix[
+        (bonds['ai'] == atom_number)
+        | (bonds['aj'] == atom_number)]
+
+    bond_candidates = ff_bonds.ix[
+        (ff_bonds['r'] == bond_check['r'].values[0])
+        & (ff_bonds['k'] == bond_check['k'].values[0])]
+
+    if bond_candidates.empty:
+        print("Found several candidate atomtypes based "
+              "on LJ paramters, but no perfect bond mathches.\n")
+
+        # quick last ditch assignment based on name
+        if row['name'].upper() in candidates['name'].values:
+            print("Approximate name match found. Would you like "
+                  "to assign atomtype '{}' from the force field "
+                  "to your atom(s) {}?".format(
+                  row['name'].upper(), atom_names))
+            choice = input("(y/n): ")
+            if choice == 'y':
+                atoms.loc[atoms['type'] == row['name'], 'ff_name'] = row['name'].upper()
+                continue
+
+        print("Here is a list of other candidates.")
+        print("Please choose an atom name for '{}' "
+              "from the following list:".format(row['name']))
+        for idx, name in enumerate(candidates['name'].values):
+            print('{}. {}'.format(idx, name))
+        selection = int(input('number: '))
+        atom_types.loc[index, 'ff_name'] = candidates['name'].values[selection]
+
+# Alright.... let's say we've got atom types correctly assigned by this point.
+# We'll have to do some manual editing, but we're close enough for government
+# work. Now to format these things in the correct group.
+
+for new_residue in atoms['restype'].unique():
+    res_df = atoms.loc[atoms['restype'] == new_residue, :]
+    header = '[ {} ]\n [ atoms ]\n'.format(new_residue)
+    res_string = [header]
+    for index, row in res_df.iterrows():
+        start_index = res_df.index[0]
+        name = row['atom']
+        ff_type = row['ff_name']
+        charge = row['charge']
+        id = row['nr'] - start_index
+
+        row_string = f'{name:>6}    {ff_type:<3}{charge:17.5f} {id:5}\n'
+        res_string.append(row_string)
+
+    res_string.append(' [ bonds ]\n')
+
+    for index, row in bonds.iterrows():
+        atom1 = atoms.loc[atoms['nr'] == row['ai'], 'atom'].values[0]
+        atom2 = atoms.loc[atoms['nr'] == row['aj'], 'atom'].values[0]
+
+        if (atom1 in res_df['atom'].values) \
+                & (atom2 in res_df['atom'].values):
+
+            row_string = f'{atom1:>6} {atom2:>5}\n'
+            res_string.append(row_string)
+
+        elif atom1 in res_df['atom'].values:
+            atom2 = '+' + atom2
+            row_string = f'{atom1:>6} {atom2:>5}\n'
+            res_string.append(row_string)
+        elif atom2 in res_df['atom'].values:
+            atom1 = '-' + atom1
+            row_string = f'{atom1:>6} {atom2:>5}\n'
+            res_string.append(row_string)
+
+    res_top = ''.join(res_string)
+    outfile = "{}_top.itp".format(new_residue)
+    full_out_path = op.join(data_dir, outfile)
+
+    with open(full_out_path, "w") as text_file:
+        print(res_top, file=text_file)
+

c3204cc6f3bc0bc51b309fa4b34d9486d0acb560
Josh <jsmith07@uw.edu>
2018-05-04 16:26:13 -0700

    Parse input conf to prepare for polymerization.

--- polymerize.py
+++ polymerize.py
@@ -5,5 +5,27 @@ import os.path as op
 data_dir = op.join(op.dirname(__file__), 'conf_data/')
 conf_file = op.join(data_dir, 'PEG.pdb')
 
-# read into mdtraj
+# unique residue types in conf_file
+start_cap_name = 'mPEG'
+repeat_name = 'PEG'
+end_cap_name = 'PEGo'
+
+# three reference atoms to translate and rotate monomer
+# to polymerization
+ref_atom_names = ['O', 'C1', 'C2']
+# read conf_file with mdtraj
 monomer = md.load(conf_file)
+coordinates = monomer.xyz[0]
+topology = monomer.topology
+
+# select atom IDs for each unique segment
+start_cap = topology.select(f'resname {start_cap_name}')
+repeat = topology.select(f'resname {repeat_name}')
+end_cap = topology.select(f'resname {end_cap_name}')
+
+ref_atoms = [coordinates[topology.select('resname {} and name {}'
+                                         ''.format(repeat_name, ref_atom))]
+             for ref_atom in ref_atom_names]
+
+# TODO: polymerize
+

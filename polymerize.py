1bd1e247fb1a9bf2644ba5a3331f52fed407f180
Josh <jsmith07@uw.edu>
2018-05-08 09:25:31 -0700

    Fix pdb string format. Polymerize working wooo.

--- polymerize.py
+++ polymerize.py
@@ -13,7 +13,7 @@ outfile = 'polymer.pdb'
 # unique residue types in conf_file
 start_cap_name = 'mPEG'
 repeat_name = 'PEG'
-n = 30  # number of repeat monomers
+n = 5  # number of repeat monomers
 flip_repeat = np.pi  # angle to rotate each repeat unit
 end_cap_name = 'PEGo'
 
@@ -36,12 +36,11 @@ end_cap = topology.select(f'resname {end_cap_name}')
 ref_atoms = [topology.select(f'{ref_atom}')
              for ref_atom in ref_atom_names]
 
-# TODO: POLYMERIZE
-# translate so the first ref atom is at the origin
+# POLYMERIZE
+# 1. translate so the first ref atom is at the origin
 coordinates -= coordinates[ref_atoms[0]]
 
-
-# pivot around ref_atom[0] so third ref atom is on the x axis z=0
+# 2. pivot around ref_atom[0] so third ref atom is on the x axis z=0
 def rotate(coords, axis, theta):
     """
     Return the new coordinates after counterclockwise rotation about
@@ -69,7 +68,7 @@ angle = math.acos(np.dot(original, target)
 # rotate coordinates
 coordinates = rotate(coordinates, normal, angle)
 
-# rotate so second ref atom has z=0
+# 3. rotate so second ref atom has z=0
 original = np.array([0,
                      coordinates[ref_atoms[1]][0, 1],
                      coordinates[ref_atoms[1]][0, 2]])
@@ -82,17 +81,17 @@ angle = math.acos(np.dot(original, target)
 coordinates = rotate(coordinates, normal, angle)
 
 # add positional data to table
-table['x'] = coordinates[:, 0]
-table['y'] = coordinates[:, 1]
-table['z'] = coordinates[:, 2]
+table['x'] = coordinates[:, 0] * 10
+table['y'] = coordinates[:, 1] * 10
+table['z'] = coordinates[:, 2] * 10
 
 # list to hold residue dataframes
 my_polymer = [table.iloc[start_cap]]
 
 # get x and y values for repeat translation
 x_shift, y_shift, _ = (coordinates[ref_atoms[2]]
-                       - coordinates[ref_atoms[1]])[0]
-# append repeats to my_polymer
+                       - coordinates[ref_atoms[1]])[0] * 10
+# 4. append repeats to my_polymer
 for repeat_idx in range(0, n):
     indices = [i + repeat_idx * len(repeat)
                for i in repeat]
@@ -117,17 +116,17 @@ for repeat_idx in range(0, n):
     new_monomer.resSeq += repeat_idx
     my_polymer.append(new_monomer)
 
-# Add capping group
+# 5. add capping group
 indices = [i + (n - 1) * len(repeat)
            for i in end_cap]
 # copy end_cap info from original table
 new_cap = pd.DataFrame(table.iloc[end_cap].values,
-                           index=indices,
-                           columns=table.columns)
+                       index=indices,
+                       columns=table.columns)
 
 # translate the coordinates, and flip if necessary
 new_cap.x += x_shift * (n - 1)
-if n % 2 == 1:
+if n % 2 == 0 and flip_repeat:
     # translate by y distance from reference axis
     new_cap.y += y_shift
     # flip around ref2 - ref0 axis
@@ -144,12 +143,11 @@ new_cap.resSeq += (n - 1)
 my_polymer.append(new_cap)
 polymer = pd.concat(my_polymer)
 
-# todo: fix string format for pdb
-pdb_lines = [f'ATOM{     a.serial}{   a.name}{ a.resName}{    a.resSeq}{      a.x}{ a.y}{   a.z}'
-             for idx, a in polymer.iterrows()]
+pdb_lines = [f'ATOM  {atom.serial:>5} {atom.name:<4} {atom.resName:>4} '
+             f'{atom.resSeq:>4}  {atom.x:>8.3f}{atom.y:>8.3f}{atom.z:>8.3f}'
+             for atom in polymer.itertuples()]
+
 pdb_string = '\n'.join(pdb_lines)
-
-
 full_out_path = op.join(data_dir, outfile)
 with open(full_out_path, "w") as text_file:
     print(pdb_string, file=text_file)
